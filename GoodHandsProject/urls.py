5a6ac54be1743ed47d160c60636ff1cfd608beab
JankoLoL <adam.chch86@gmail.com>
2023-06-27 12:44:57 +0200

    Landing page

--- GoodHandsProject/urls.py
+++ GoodHandsProject/urls.py
@@ -14,9 +14,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+
+from good_hands_app.views import LandingPageView, AddDonationView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', LandingPageView.as_view(), name='landing-page'),
+    path('donation/', AddDonationView.as_view(), name='donation'),
+
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
