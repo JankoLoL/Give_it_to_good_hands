f8b837d3fdac48f55852005b4515cec61be713d3
JankoLoL <adam.chch86@gmail.com>
2023-06-28 12:27:01 +0200

    model Category, Institution

--- good_hands_app/models.py
+++ good_hands_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django import forms
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=64)
+
+
+class Institution(models.Model):
+    INSTITUTION_TYPE = [
+        ("fundacja", "fundacja"),
+        ("organizacja pozarządowa", "organizacja pozarządowa"),
+        ("zbiórka lokalna", "zbiórka lokalna"),
+    ]
+    name = models.CharField(max_length=64)
+    description = models.TextField()
+    type = forms.ChoiceField(choices=INSTITUTION_TYPE, initial="fundacja")
+    categories = models.ManyToManyField(Category)
