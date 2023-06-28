2bae66c435456ba1dbbada8c350b01f01270fc13
JankoLoL <adam.chch86@gmail.com>
2023-06-28 13:37:36 +0200

    model Donation

--- good_hands_app/models.py
+++ good_hands_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django import forms
+from django.contrib.auth.models import User
 
 
 class Category(models.Model):
@@ -16,3 +17,17 @@ class Institution(models.Model):
     description = models.TextField()
     type = forms.ChoiceField(choices=INSTITUTION_TYPE, initial="fundacja")
     categories = models.ManyToManyField(Category)
+
+
+class Donation(models.Model):
+    quantity = models.IntegerField()
+    categories = models.ManyToManyField(Category)
+    institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
+    address = models.CharField(max_length=128)
+    phone_number = models.IntegerField()
+    city = models.CharField(max_length=64)
+    zip_code = models.CharField(max_length=10)
+    pick_up_date = models.DateField()
+    pick_up_time = models.TimeField()
+    pick_up_comment = models.TextField()
+    user = models.ForeignKey("auth.User", null=True, default=True, on_delete=models.CASCADE)
